8d012be93f369338d1c6b57ca08fa59107df4a3e
labmouse41 <107990826+labmouse41@users.noreply.github.com>
2023-04-06 10:36:27 +0900

    add pawn

---
+++ Pawn.h
@@ -0,0 +1,18 @@
+#pragma once
+class Pawn
+{
+public:
+	void SetPosition(float x, float y)
+	{}
+	void AddPosition(float x, float y)
+	{}
+	void Damage(float Damage)
+	{}
+
+private:
+	float posX;
+	float posY;
+	float Damage;
+};
+
+
