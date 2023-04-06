db69140e48d5ea354f1e9ca6ce5b0e8d6fec8fab
labmouse41 <107990826+labmouse41@users.noreply.github.com>
2023-04-06 10:16:05 +0900

    파일 구분

--- main.cpp
+++ main.cpp
@@ -1,34 +1,9 @@
+#include <iostream>
+using namespace std;
 
-class Pawn
+int main()
 {
-public:
-	void SetPosition(float x, float y)
-	{}
-	void AddPosition(float x, float y)
-	{}
-	void Damage(float Damage, class pawn)
-	{}
 
-private:
-	float posX;
-	float posY;
-	float Damage;
-};
-
-class Slime : public Pawn
-{};
-
-class Goblin : public Pawn
-{};
-
-class Bore : public Pawn
-{};
-
-class player : public Pawn
-{};
-
-
-
-
-
+	return 0;
+}
 
